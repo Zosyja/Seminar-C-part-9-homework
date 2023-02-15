89174c7d24c357f2aed0b15a4865844dc85400e2
Zosyja <zosyja@bk.ru>
2023-02-15 22:38:16 +0300

    added Task005

---
+++ Task005/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму цифр числа
+
+int SumDigits (int A){
+    if (A==0) return 0;
+    else return A%10 + SumDigits(A/10);
+}
+
+Console.Write("Введите натуральное число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Сумма цифр числа = ");
+Console.Write(SumDigits(N));
