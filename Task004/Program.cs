e9baf0942f6fcb9088156a788041c627a46bb359
Zosyja <zosyja@bk.ru>
2023-02-15 22:30:11 +0300

    added Task004

---
+++ Task004/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int SumRec (int A, int B, int step){
+    if (A==B+step) return 0;
+    else return A + SumRec(A+step,B, step);
+}
+
+Console.Write("Введите натуральное число M: ");
+int M = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите натуральное число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+int step = 0;
+if (M<N) step = 1;
+else step = -1;
+
+Console.WriteLine(SumRec(M,N,step));
