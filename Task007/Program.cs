c0069825f24e313ce0e3dd786f255c036ae14739
Zosyja <zosyja@bk.ru>
2023-02-15 23:10:41 +0300

    added Task007

---
+++ Task007/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу возведения числа А в целую степень B
+
+int PowerRec (int A, int B){
+    if (B==0) return 1;
+    else return PowerRec (A,B-1)*A;
+}
+
+Console.Write("Введите натуральное число A: ");
+int A = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите натуральное число B: ");
+int B = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write($"Число {A} в степени {B} = ");
+Console.WriteLine(PowerRec(A,B));
