36d92d6cce73353805a085f0fc27f991a1512f4d
Zosyja <zosyja@bk.ru>
2023-02-15 20:31:14 +0300

    added Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+
+string NumbersRec (int i, int N){
+    if (i <= N) return $"{i} " + NumbersRec (i+1, N);
+    else return String.Empty;
+}
+
+Console.Write("Введите натуральное число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine(NumbersRec(1,N));
+
